ae5eb267ef9872bd5a616ac6e79a844f7b512387
dtru0010 <dtru0010@student.monash.edu>
2024-10-06 21:54:51 +1100

    add: homework 10 chart vis

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,2 +1,5 @@
 var vg_1 = "./visualisations/enerConsumPcap.json";
 vegaEmbed("#aus_ener_consum_pcap", vg_1).then(function(result){}).catch(console.error);
+
+var vg_2 = "./visualisations/totalEnerConsumStateFuel.json";
+vegaEmbed("#aus_ener_consum_fuel_state", vg_2).then(function(result){}).catch(console.error);
