002c5993a61f938c24bd9d0e2507454c1620a2fc
dtru0010 <dtru0010@student.monash.edu>
2024-10-11 06:50:04 +1100

    add: temp layout of visualisation

---
+++ visualisation2/vega_lite_vis.js
@@ -0,0 +1,14 @@
+var vg_1 = "./visualisations/ausEnergyProduction.json";
+vegaEmbed("#production", vg_1, {actions: false}).then(function(result){}).catch(console.error);
+
+var vg_2 = "./visualisations/ausEnergyConsumFuelState.json";
+vegaEmbed("#consumption", vg_2, {actions: false}).then(function(result){}).catch(console.error);
+
+var vg_4 = "./visualisations/ausNetZeroTracking.json";
+vegaEmbed("#netzero", vg_4, {actions: false}).then(function(result){}).catch(console.error);
+
+var vg_5 = "./visualisations/ausEnergyMixPortion.json";
+vegaEmbed("#mix", vg_5, {actions: false}).then(function(result){}).catch(console.error);
+
+var vg_6 = "./visualisations/worldShareFuelConsumPercent.json";
+vegaEmbed("#world", vg_6, {actions: false}).then(function(result){}).catch(console.error);
